0869079505ca3b345a83c74d3dde1933b614a3e5
Ubuntu <ubuntu@ip-172-31-90-196.ec2.internal>
2019-04-10 01:21:06 +0000

    audio se guarda en la carpeta audios

--- radioscrap.py
+++ radioscrap.py
@@ -7,9 +7,12 @@ Este es un archivo temporal.
 
 from urllib import request
 import time
+import os
 response = request.urlopen("https://18493.live.streamtheworld.com/RADIO_RPP.mp3")
+folder ='audios'
 filename = time.strftime("%Y%m%d%H%M%S",time.localtime())+".wav"
-f = open(filename, 'wb')
+x_filename = os.path.join(folder,filename)
+f = open(x_filename, 'wb')
 start_time_in_seconds = time.time()
 time_limit = 10
 block_size = 1024
